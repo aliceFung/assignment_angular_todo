416ee9f5c2cdbc595746d621657fb476c2faa959
Alice Fung <alice.o.fung@gmail.com>
2015-09-15 12:53:22 -0400

    form creation

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 var app = angular.module("todo",[]);
 
-app.controller("TodoCtrl", ["$scope", function($scope){
+app.controller("TodoCtrl", ["$scope", "$window", function($scope, $window){
 
-  $scope.items = [];
-  $scope.item = { text: "Get groceries from the store",
+  $scope.createItem = function(){
+    console.log($scope.item.text + " due " + $scope.item.dueDate);
+    $scope.items.push($scope.item);
+
+    // clearing input
+    $scope.item = { text: "",
+                dueDate: new Date(),
+                completed: false };
+    // $window.alert('something pops up');
+  };
+
+  $scope.items = [
+  { text: "Get groceries from the store",
+                dueDate: new Date(),
+                completed: false },
+
+  { text: "Get more stuff from the store",
+  dueDate: new Date(),
+  completed: false },
+
+  { text: "Get even more stuff from the store",
+                dueDate: new Date(),
+                completed: false }
+  ];
+
+  $scope.item = { text: "",
                 dueDate: new Date(),
                 completed: false };
 
