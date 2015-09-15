7b42fa726a2b588cb8e7969335008e13a531948b
Alice Fung <alice.o.fung@gmail.com>
2015-09-15 14:40:00 -0400

    reorganized directory

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Do it!
 
 [An AngularJS JavaScript app from the Viking Code School involving CRUD actions, custom directives, custom services, custom filters and all the usual goodness](http://www.vikingcodeschool.com)
+
+[Alice Fung](https://github.com/aliceFung/assignment_angular_todo) & [Mike Butts](https://github.com/mikebutts/assignment_angular_todo)
---
+++ javascripts/controllers/TodoCtrl.js
@@ -0,0 +1,57 @@
+app.controller("TodoCtrl", ["$scope", "$window", function($scope, $window){
+
+  $scope.createItem = function(){
+    console.log($scope.item.text + " due " + $scope.item.dueDate + " completed: " + $scope.item.completed);
+    $scope.items.push($scope.item);
+
+    // clearing input
+    $scope.item = { text: "",
+                dueDate: new Date(),
+                completed: false };
+    // $window.alert('something pops up');
+  };
+
+  $scope.deleteItem = function(index){
+        $scope.items.splice(index,1);
+  };
+
+  $scope.clearCompleted = function(){
+    //selects subset of array and returns new array
+    $scope.items = $scope.items.filter(function(item){
+        //keep whatever you return
+        return !item.completed;
+    });
+  };
+
+  $scope.toggleCompletedDisplay = function(){
+    $scope.completion.display = !($scope.completion.display);
+    if ($scope.completion.display){
+      $scope.completion.text = "Hide Completed";
+    } else {
+       $scope.completion.text = "Show Completed";
+    }
+  };
+
+  //if true, completed task are shown
+  $scope.completion = { display: true,
+                        text: "Hide Completed"};
+
+  $scope.items = [
+  { text: "Get groceries from the store",
+                dueDate: new Date(),
+                completed: true },
+
+  { text: "Get more stuff from the store",
+  dueDate: new Date(),
+  completed: false },
+
+  { text: "Get even more stuff from the store",
+                dueDate: new Date(),
+                completed: false }
+  ];
+
+  $scope.item = { text: "",
+                dueDate: new Date(),
+                completed: false };
+
+}]);
